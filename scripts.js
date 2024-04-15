d46bd8a94078ccfa597fd4d40968b25cd8e91cb5
victorHF <victorhf1313@gmail.com>
2024-04-15 12:00:04 -0300

    new commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      }
+
+
+
+  });
